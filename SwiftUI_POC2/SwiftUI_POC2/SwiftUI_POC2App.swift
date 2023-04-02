35cbd221eea29faad8179e32c9d04b11e2fa464e
Dipak Makwana <dipakbhai.makwana@multichoice.com>
2023-04-02 21:27:22 +0530

    Project Files added

---
+++ SwiftUI_POC2/SwiftUI_POC2/SwiftUI_POC2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_POC2App.swift
+//  SwiftUI_POC2
+//
+//  Created by Dipakbhai Valjibhai Makwana on 02/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_POC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
